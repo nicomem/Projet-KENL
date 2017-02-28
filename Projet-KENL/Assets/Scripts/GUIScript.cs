8860e37ed6cb1f9ee34b7f9310ad59a28d82be8d
MEMEINT_N <nicolas.memeint@epita.fr>
2017-02-28 18:49:59 +0100

    menu

---
+++ Projet-KENL/Assets/Scripts/GUIScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIScript : MonoBehaviour {
+    private bool isPaused;
+	// Use this for initialization
+	void Start () {
+        isPaused = false;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            isPaused = !isPaused;
+        if (isPaused)
+            Time.timeScale = 0f;
+        else
+            Time.timeScale = 1.0f;
+    }
+    private void OnGUI()
+    {
+        if (isPaused) 
+        {
+            if (GUI.Button(new Rect(Screen.width / 2 - 320, Screen.height / 2 - 120, 640, 80), "Continue"))
+                isPaused = false;
+            if (GUI.Button(new Rect(Screen.width / 2 - 320, Screen.height / 2 + 80, 640, 80), "Back to main menu" ))
+                {
+                //Application.Quit();
+                Application.LoadLevel("MainMenu");
+            }
+        }
+ 
+    }
+}
