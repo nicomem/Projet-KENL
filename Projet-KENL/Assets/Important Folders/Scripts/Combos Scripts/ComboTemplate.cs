be49355f894f0321d1782b98740263f0e43d6941
MEMEINT_N <nicolas.memeint@epita.fr>
2017-04-25 00:03:01 +0200

    bug fixes => merge

--- Projet-KENL/Assets/Important Folders/Scripts/Combos Scripts/ComboTemplate.cs
+++ Projet-KENL/Assets/Important Folders/Scripts/Combos Scripts/ComboTemplate.cs
@@ -48,6 +48,9 @@ public class ComboTemplate : MonoBehaviour
 
             // No self-hitting
             if (playerHit.transform.GetInstanceID() != transform.GetInstanceID()) {
+
+                Debug.Log(playerHit.transform.name);
+
                 leftRight = GetComponent<PlayerScript>()
                     .LookToRight() ? 1f : -1f;
 
