df1e4f8aef2257e3bd940712a8d560033e2d507e
MEMEINT_N <nicolas.memeint@epita.fr>
2017-06-09 18:28:26 +0200

    Added Vlasimi X perso

--- Projet-KENL/Assets/Important Folders/Personnages/Adventurer/Scripts/mipmap.js
+++
@@ -1,4 +0,0 @@
-var mipmap : float = -0.5;
-function Start () {
-	renderer.material.mainTexture.mipMapBias = mipmap;
-}
--- Projet-KENL/Assets/Important Folders/Personnages/Adventurer/Scripts/mouseSpin.js
+++
@@ -1,6 +0,0 @@
-
-function Update () {
-	if(Input.GetMouseButton(0)){
-		transform.rotation.eulerAngles.y += Input.GetAxis("Mouse X") * Time.deltaTime * -100.0;
-	}
-}
