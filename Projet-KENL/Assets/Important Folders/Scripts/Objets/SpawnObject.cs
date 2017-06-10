0ff94a5f2c19ee9e8721a9b83eb7bdf05ba0b7f4
MEMEINT_N <nicolas.memeint@epita.fr>
2017-06-10 14:08:30 +0200

    objects

--- Projet-KENL/Assets/Important Folders/Scripts/Objets/SpawnObject.cs
+++ Projet-KENL/Assets/Important Folders/Scripts/Objets/SpawnObject.cs
@@ -1,57 +1,45 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class SpawnObject : MonoBehaviour {
-
-    public GameObject ObjetAttaque;
-    public GameObject ObjetInvert;
-    public GameObject ObjetPV;
-    public GameObject ObjetVitesse;
-    public GameObject ObjetVitesseMalus;
+public class SpawnObject : MonoBehaviour
+{
+    public GameObject[] ObjectPrefabs;
 
     private float time;
     private float spawnTime = 5f;
-
-    private int test = 0;
+    
     public Vector3 positionToSpawn;
 
-    System.Random rand = new System.Random();
+    private GameObject objectSpawned;
+
+    System.Random rand;
+
 	// Use this for initialization
 	void Start ()
     {
-        positionToSpawn = new Vector3(0, 8, 0); 
-        whatPrefab();
+        rand = new System.Random();
+        positionToSpawn = new Vector3(0, 8, 0);
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate ()
     {   
-        //if (time >= spawnTime)
-        //compte
-        time += Time.deltaTime;
+        // Do not spawn multiple objects at the same time
+        if (objectSpawned == null)
+            time += Time.deltaTime;
 
-        //Check si cst le bon moment de spawn les objets
+        // Check si cst le bon moment de spawn les objets
         if (time >= spawnTime)
         {
             time = 0;
-            if (test == 1)
-                Instantiate(ObjetAttaque, positionToSpawn, Quaternion.identity);
-            else if (test == 2)
-                Instantiate(ObjetPV, positionToSpawn, Quaternion.identity);
-            else if (test == 3)
-                Instantiate(ObjetInvert, positionToSpawn, Quaternion.identity);
-            else if (test == 4)
-                Instantiate(ObjetVitesse, positionToSpawn, Quaternion.identity);
-            else if (test == 5)
-                Instantiate(ObjetVitesseMalus, positionToSpawn, Quaternion.identity);
-            whatPrefab();
+            int indexObject = GetObjectIndexRandom();
+
+            objectSpawned = Instantiate(ObjectPrefabs[indexObject], positionToSpawn,
+                Quaternion.identity);
         }
     }
 
-    void whatPrefab()
+    private int GetObjectIndexRandom()
     {
-        test = rand.Next(1, 6);
+        return rand.Next(ObjectPrefabs.Length);
     }
 }
